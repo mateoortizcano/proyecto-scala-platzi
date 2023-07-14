36e3cf803ff57f3a0f20518a0c0c4e0c55d6b3c4
Mateo Ortiz cano - Ceiba Software <mateo.ortiz@ceiba.com.co>
2023-07-13 19:14:59 -0500

    Implemento los métodos del CRUD

--- app/repository/MovieRepository.scala
+++ app/repository/MovieRepository.scala
@@ -1,6 +1,6 @@
 package repository
 
-import models.MovieTable
+import models.{Movie, MovieTable}
 
 import javax.inject.Inject
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
@@ -34,15 +34,36 @@ with HasDatabaseConfigProvider[JdbcProfile] {
     db.run(createSchema)
   }
 
-  def getAll = ???
+  def getAll = {
+    val q = movieQuery.sortBy(_.id)
+    db.run(q.result)
+  }
 
-  def getOne = ???
+  def getOne(id: String) = {
+    val q = movieQuery.filter(_.id === id)
+    db.run(q.result.headOption)
+  }
 
-  def create = ???
+  def create(movie: Movie) = {
+    val insert = movieQuery += movie
+    db.run(insert)
+      .flatMap(_ => getOne(movie.id.getOrElse("")))
+  }
 
-  def update = ???
+  def update(id: String, movie: Movie) = {
+    val q = movieQuery.filter(_.id === movie.id && movie.id.contains(id))
+    val update = q.update(movie)
+    db.run(update)
+      .flatMap(_ => db.run(q.result.headOption))
+  }
 
-  def delete = ???
+  def delete(id: String) = {
+    val q = movieQuery.filter(_.id === id)
+    for {
+      objeto <- db.run(q.result.headOption)
+      _ <- db.run(q.delete)
+    } yield objeto
+  }
 }
 
 
